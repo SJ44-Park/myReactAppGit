99f7c8b4507fe58029db09df67c3d052c7ab362f
SJ44-Park <tutor4microcom@gmail.com>
2022-07-02 21:57:53 +0900

    Create React Project

---
+++ myweb/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>my First Reat Web App Page </h2>
+    </div>
+  );
+}
+
+export default App;
